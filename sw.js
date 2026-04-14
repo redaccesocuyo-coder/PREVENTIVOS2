9b0cf61bb2a4e8f85736a4f9e14029d67c6caf07
redaccesocuyo-coder <redaccesocuyo@gmail.com>
2026-04-14 10:33:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,38 @@
-const CACHE_NAME = 'preven-v1';
+const CACHE_NAME = 'preven-v2';
+
 const ASSETS = [
   './',
   './index.html',
   './style.css',
-  './icon.png',
-  'https://cdn.sheetjs.com/xlsx-0.20.1/package/dist/xlsx.full.min.js'
+  './icon.png'
 ];
 
-// Instalar Service Worker
+// INSTALL
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
-// Activar Service Worker
+// ACTIVATE
 self.addEventListener('activate', event => {
-  console.log('Service Worker activado');
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-// Estrategia de Cache
+// FETCH
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
   );
 });
